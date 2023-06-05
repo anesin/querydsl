ecd135501aa266f6aa7764a504a5b7acd84f8279
이재영 <jaeyoung.yi@seculetter.com>
2023-06-05 15:26:35 +0900

    섹션 3. 기본 문법: Case 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 import com.querydsl.core.NonUniqueResultException;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -225,6 +226,30 @@ public class QuerydslBasicTest {
     assertThat(result)
         .extracting("username")
         .containsExactly("teamA", "teamB");
+  }
+
+
+  @Test
+  void basicCase() {
+    factory.select(member.age
+                         .when(10).then("열살")
+                         .when(20).then("스무살")
+                         .otherwise("기타"))
+           .from(member)
+           .fetch()
+           .forEach(s -> System.out.println("s = " + s));
+  }
+
+
+  @Test
+  void complexCase() {
+    factory.select(new CaseBuilder()
+                    .when(member.age.between(0, 20)).then("0~20살")
+                    .when(member.age.between(21, 30)).then("21~30살")
+                    .otherwise("기타"))
+            .from(member)
+            .fetch()
+            .forEach(s -> System.out.println("s = " + s));
   }
 
 }
