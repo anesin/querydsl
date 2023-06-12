66d37a747ca759be14d0c2090b33f00cf95229c3
이재영 <jaeyoung.yi@seculetter.com>
2023-06-12 09:30:50 +0900

    섹션 4. 중급 문법 : 동적 쿼리 - Where 다중 파라미터 사용

--- src/test/java/study/querydsl/QuerydslIntermediateTest.java
+++ src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -165,6 +166,39 @@ public class QuerydslIntermediateTest {
     return factory.selectFrom(member)
                   .where(builder)
                   .fetch();
+  }
+
+
+  @Test
+  void dynamicQuery_WhereParam() {
+    String usernameParam = "member1";
+    Integer ageParam = null;
+
+    List<Member> result = searchMember2(usernameParam, ageParam);
+    assertThat(result.size()).isEqualTo(1);
+  }
+
+
+  private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+    return factory.selectFrom(member)
+//                  .where(usernameEq(usernameCond), ageEq(ageCond))
+                  .where(allEq(usernameCond, ageCond))
+                  .fetch();
+  }
+
+
+  private BooleanExpression usernameEq(String usernameCond) {
+    return usernameCond == null? null: member.username.eq(usernameCond);
+  }
+
+
+  private BooleanExpression ageEq(Integer ageCond) {
+    return ageCond == null? null: member.age.eq(ageCond);
+  }
+
+
+  private BooleanExpression allEq(String usernameCond, Integer ageCond) {
+    return usernameEq(usernameCond).and(ageEq(ageCond));
   }
 
 }
