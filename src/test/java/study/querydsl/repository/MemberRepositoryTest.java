42f3cc7387691710549c370b4b54c9090f28025a
이재영 <jaeyoung.yi@seculetter.com>
2023-06-22 09:58:15 +0900

    섹션 7. 스프링 데이터 JPA가 제공하는 Querydsl 기능: 인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -15,6 +15,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 
 @SpringBootTest
@@ -46,19 +47,7 @@ class MemberRepositoryTest {
 
   @Test
   void searchTest() {
-    Team teamA = new Team("teamA");
-    Team teamB = new Team("teamB");
-    em.persist(teamA);
-    em.persist(teamB);
-
-    Member member1 = new Member("member1", 10, teamA);
-    Member member2 = new Member("member2", 20, teamA);
-    Member member3 = new Member("member3", 30, teamB);
-    Member member4 = new Member("member4", 40, teamB);
-    em.persist(member1);
-    em.persist(member2);
-    em.persist(member3);
-    em.persist(member4);
+    readyData();
 
     MemberSearchCondition condition = new MemberSearchCondition();
     condition.setAgeGoe(35);
@@ -71,19 +60,7 @@ class MemberRepositoryTest {
 
   @Test
   void searchPageTest() {
-    Team teamA = new Team("teamA");
-    Team teamB = new Team("teamB");
-    em.persist(teamA);
-    em.persist(teamB);
-
-    Member member1 = new Member("member1", 10, teamA);
-    Member member2 = new Member("member2", 20, teamA);
-    Member member3 = new Member("member3", 30, teamB);
-    Member member4 = new Member("member4", 40, teamB);
-    em.persist(member1);
-    em.persist(member2);
-    em.persist(member3);
-    em.persist(member4);
+    readyData();
 
     MemberSearchCondition condition = new MemberSearchCondition();
     PageRequest pageRequest = PageRequest.of(0, 3);
@@ -98,6 +75,32 @@ class MemberRepositoryTest {
     assertThat(resultsComplex)
         .extracting("username")
         .containsExactly("member1", "member2", "member3");
+  }
+
+
+  @Test
+  void querydslPredicateExecutorTest() {
+    readyData();
+
+    memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")))
+                    .forEach(System.out::println);
+  }
+
+
+  private void readyData() {
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
   }
 
 }
